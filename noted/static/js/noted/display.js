89e387f8ae25807f5f8033b97696fbd8e60746ed
David Eidelman <eidelman.david@gmail.com>
2022-12-03 07:15:30 -0500

    moved editor creation to noted/display.js

---
+++ noted/static/js/noted/display.js
@@ -0,0 +1,34 @@
+/// provides the specific javascript routines for the display endpoint
+
+const create_editor = (elementName) => {
+  let editor = monaco.editor.create(document.getElementById(elementName), {
+    language: "markdown-math",
+    value: "",
+    minimap: {
+      enabled: false,
+    },
+    fontSize: 16,
+    //   theme: "vs-dark",
+    theme: "default",
+    // readOnly: "true",
+  });
+  require(["MonacoMarkdown"], function (MonacoMarkdown) {
+    const extension = new MonacoMarkdown.MonacoMarkdownExtension();
+    extension.activate(editor);
+  });
+  return editor;
+};
+
+// display_note
+//
+// initializes the editor component
+//
+// parameters:
+//  editor: MonacoEditor
+//  noteBody: String
+//  readOnly: bool
+//
+const display_note = (editor, noteBody, readOnly) => {
+  editor.getModel().setValue(noteBody);
+  editor.updateOptions({ readOnly: readOnly });
+};
